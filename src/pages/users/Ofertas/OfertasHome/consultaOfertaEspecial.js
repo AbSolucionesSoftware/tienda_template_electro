5bfd0022012d819d4795b39f66fc08353ca1eda1
poday98 <poday736.98@gmail.com>
2021-01-20 16:41:02 -0600

    cambios a medias

--- src/pages/users/Ofertas/OfertasHome/consultaOfertaEspecial.js
+++ src/pages/users/Ofertas/OfertasHome/consultaOfertaEspecial.js
@@ -4,9 +4,13 @@ import { Result, Row } from 'antd';
 import {withRouter } from 'react-router-dom';
 import '../ofertas.scss';
 import ComponenteProductoGrande from '../../Productos/Card_Grande/componente_producto_grande';
+import CardSecundaria from '../../Productos/Card_Secundaria/card_secundaria';
+
 import Spin from '../../../../components/Spin';
 
 function ConsultaOfertasEspecial(props) {
+	const {orientacion} = props;
+	
 	const [ productos, setProductos ] = useState([]);
 	const [ loading, setLoading ] = useState(false);
 
@@ -17,7 +21,7 @@ function ConsultaOfertasEspecial(props) {
 	async function obtenerProductos() {
 		setLoading(true);
 		await clienteAxios
-			.get(`/productos/promociones?limit=${1}&page=${1}`)
+			.get(`/productos/promociones?limit=${5}&page=${1}`)
 			.then((res) => {
 				setProductos(res.data.posts.docs);
 				setLoading(false);
@@ -27,9 +31,24 @@ function ConsultaOfertasEspecial(props) {
 			});
 	}
 
-	const render = productos.map((productos) => (
-		<ComponenteProductoGrande key={productos._id} productos={productos} />
-	));
+	const render = productos.map((productos, index) => {
+		if(orientacion == "derecha"){
+			if(index === 4){
+				return  <ComponenteProductoGrande key={productos._id} productos={productos} />
+			}else{
+				return <CardSecundaria  key={productos._id} productos={productos}/>
+			}
+		}else{
+
+		if(index === 0){
+			return  <ComponenteProductoGrande key={productos._id} productos={productos} />
+		}else{
+			return <CardSecundaria  key={productos._id} productos={productos}/>
+		}
+	}
+
+	});
+		
 
 	if(productos.length === 0){
 		return null;
--- src/pages/users/Ofertas/OfertasHome/ofertasDerecha.js
+++ src/pages/users/Ofertas/OfertasHome/ofertasDerecha.js
@@ -11,7 +11,7 @@ function  OfertasDerecha() {
     return (
 		<div className="d-flex justify-content-center align-items-center">
         <Row gutter={10} style={{ maxWidth: '95vw' }} className=" mt-4">
-            <div className="col-lg-9">
+            {/* <div className="col-lg-9">
                 <Tabs defaultActiveKey="1" className="tabs-colors" centered style={{ fontSize: 18 }}>
                     <TabPane tab="Ofertas" key="1" >
                         <ConsultaOfertas />
@@ -22,9 +22,9 @@ function  OfertasDerecha() {
                         Ver todas las ofertas
                     </Link>
                 </div>
-            </div>
-            <div className="col-lg-3 mt-3">
-                <ConsultaOfertasEspecial />
+            </div> */}
+            <div className="col-lg-12 mt-3">
+                <ConsultaOfertasEspecial orientacion="derecha" />
             </div>
         </Row>
         </div>
